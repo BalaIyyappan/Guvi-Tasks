a84acfc4c8db8961c1b3bc6d068a0a1eaf01f00d
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 22:54:52 +0530

    Create Day using switch.py

---
+++ Task-Completed/Day using switch.py
@@ -0,0 +1,5 @@
+40.Write a program to print day of week name using switch case. 
+SOL:
+week={1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday',7:'Sunday'}
+day=int(input('Enter the week number:'))
+print(week[day])
