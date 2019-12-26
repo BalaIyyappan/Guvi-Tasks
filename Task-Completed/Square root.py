9beaa6d566ea0b334bc97f5eb46a6f939e7fffe5
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:54:54 +0530

    Create Square root.py

---
+++ Task-Completed/Square root.py
@@ -0,0 +1,6 @@
+12. Write a program to enter any number and calculate its square root.
+SOL:
+import math as m
+num=int(input("Enter any number:"))
+result=int(m.sqrt(num))
+print("Square root is:",result)
