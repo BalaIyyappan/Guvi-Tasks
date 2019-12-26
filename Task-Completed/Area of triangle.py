ee920d0a099e3b8c17dc1da610387116104284b8
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 17:31:33 +0530

    Create Area of triangle.py

---
+++ Task-Completed/Area of triangle.py
@@ -0,0 +1,6 @@
+14. Write a program to enter base and height of a triangle and find its area.
+SOL:
+b=int(input("Enter Base:"))
+h=int(input("Enter Height:"))
+area=(h*b)*0.5
+print("Area of triangle:",area)
