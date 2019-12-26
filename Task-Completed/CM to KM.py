6a6a2480bdc4bca0cc7f30394127e586cade2e2d
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:52:33 +0530

    Create CM to KM.py

---
+++ Task-Completed/CM to KM.py
@@ -0,0 +1,7 @@
+7. Write a program to enter length in centimeter and convert it into meter and kilometre
+SOL:
+l=int(input("Enter length in centimeter:"))
+meter=l/100
+kilo=meter/1000
+print("In meter:",meter,"m")
+print("In kilometer:",kilo,"Km")
