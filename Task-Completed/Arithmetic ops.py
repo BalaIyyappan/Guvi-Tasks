ef22aa8f62eb478b583f1a194dad6c8278a37998
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:50:18 +0530

    Create Arithmetic ops.py

---
+++ Task-Completed/Arithmetic ops.py
@@ -0,0 +1,14 @@
+3. Write a program to enter two numbers and perform all arithmetic operations.
+SOL:
+num1=int(input("First Number:"))
+num2=int(input("Second Number:"))
+add=num1+num2
+sub=num1-num2
+div=num1/num2
+mul=num1*num2
+mod=num1%num2
+print("Sum is:",add)
+print("Subtraction is:",sub)
+print("Division is:",div)
+print("Multiplication is:",mul)
+print("Modulus  is:",mod)
