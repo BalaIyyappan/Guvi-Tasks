12edbf85892f2a4111f054fdc1156c95c7b15d1e
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 23:14:42 +0530

    Create Sum of first and last.py

---
+++ Looping/Sum of first and last.py
@@ -0,0 +1,9 @@
+52.Write a program to find sum of first and last digit of a number. 
+SOL:
+n=input("Enter any number:")
+f=n[0]
+f=int(f)
+l=n[-1]
+l=int(l)
+s=f+l
+print('Sum of first and last digit is',s)
