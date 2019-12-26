9d2fe39a80ba6259c9e66cba671ea1c14b301bed
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 17:36:01 +0530

    Create Alphabet or not.py

---
+++ Task-Completed/Alphabet or not.py
@@ -0,0 +1,8 @@
+
+25.Write a program to check whether a character is alphabet or not. 
+SOL:
+alpha=input('Enter an alphabet:')
+if alpha in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
+  print("Yes")
+else:
+  print("No")
