d194723f45e454a22fcae262bd4fc56943a76319
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:45:36 +0530

    Create Basic Input and Output.py

---
+++ Task-Completed/Basic Input and Output.py
@@ -0,0 +1,6 @@
+1. Write a program to perform input/output of all basic data types.
+SOL:
+integer=int(input('Integer input:'))
+float_num=float(input("Float input:"))
+string=input("String input:")
+print(integer," ",float_num," ",string)
