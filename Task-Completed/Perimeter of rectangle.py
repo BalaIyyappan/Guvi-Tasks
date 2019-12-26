2a0c80c47e7b386a7b99913f16133975bf153933
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:50:54 +0530

    Create Perimeter of rectangle.py

---
+++ Task-Completed/Perimeter of rectangle.py
@@ -0,0 +1,6 @@
+4. Write a program to enter length and breadth of a rectangle and find its perimeter.
+SOL:
+num1=int(input("First Number:"))
+num2=int(input("Second Number:"))
+per=2*(num1+num2)
+print("Perimeter of rectangle: ",per)
