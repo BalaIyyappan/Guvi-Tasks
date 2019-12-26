9115daa58c34788c92fb0c9cc8ad5ff8cf589094
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 17:40:48 +0530

    Create Vowel or not.py

---
+++ Task-Completed/Vowel or not.py
@@ -0,0 +1,7 @@
+26.Write a program to input any alphabet and check whether it is vowel or consonant
+SOL:
+ch=input("Enter any character:")
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+  print(ch,"It is a vowel")
+else:
+  print(ch,"It is a consonent")
