b79fe16336af9562b1abf2b953703e98df8a22f5
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:51:34 +0530

    Create Area of rectangle.py

---
+++ Task-Completed/Area of rectangle.py
@@ -0,0 +1,6 @@
+5. Write a program to enter length and breadth of a rectangle and find its area.
+SOL:
+num1=int(input("First Number:"))
+num2=int(input("Second Number:"))
+area=(num1*num2)
+print("Area of rectangle: ",area)
