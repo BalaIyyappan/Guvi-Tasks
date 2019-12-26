e0becdd50f64d7b58cb560ecb2beb5f69f96df33
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 16:52:00 +0530

    Create Circle.py

---
+++ Task-Completed/Circle.py
@@ -0,0 +1,9 @@
+6. Write a program to enter radius of a circle and find its diameter, circumference and area.
+SOL:
+num1=float(input("Radius:"))
+area=3.14*(num1**2)
+cir=2*3.14*num1
+dia=num1*2
+print("Area of circle: ",area)
+print("Circumference of circle:",cir)
+print("Diameter of circle:",dia)
