d9fbfa2e9be035a30b3373a38d43912fb7506a43
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2019-12-26 17:45:09 +0530

    Create Upper or lower.py

---
+++ Task-Completed/Upper or lower.py
@@ -0,0 +1,10 @@
+28.Write a program to check whether a character is uppercase or lowercase alphabet. 
+SOL:
+ch=input("Enter any character:")
+
+if ch.isupper()==True:
+  print("It is in upper case")
+elif ch.iSlower()==True:
+  print("It is in lower case")
+else:
+  print("Cannot determine")
